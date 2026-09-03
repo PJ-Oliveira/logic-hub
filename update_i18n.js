58ab07f7bbf5c1ea21526c8ee99d69bdfcbab4dc
Paulo <84526032+PJ-Oliveira@users.noreply.github.com>
2026-09-03 11:11:06 -0300

    Corrige build do TypeScript

--- update_i18n.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-let file = fs.readFileSync('src/i18n.ts', 'utf8');
-
-// We need to inject new translations into propTableau
-// Let's do it cleanly by replacing the propTableau object in English and Portuguese.
-// This is fragile if we don't know the exact string, so I'll just rewrite the file.
